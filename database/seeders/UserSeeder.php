6cec032fed5653013ef58254ed720b1ef1d39bf6
geofrey-eon <geofrey-eon@users.noreply.github.com>
2025-10-03 23:14:19 +0800

    testing create and update the user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+
+
+          // Create or update an admin user
+        User::updateOrCreate(
+            ['email' => 'inigogeofrey@gmail.com'], // check if this email exists
+            [
+                'name' => 'Geofrey Inigo',
+                'password' => Hash::make('inigogeofrey'), // change to secure password
+            ]
+        );
+
+     //old code
+        // User::factory()->create([
+        //     'name' => 'Geofrey Inigo ',
+        //     'email' => 'inigogeofrey@gmail.com',
+        //     'password'=> bcrypt('inigogeofrey'),
+        // ]);
+    }
+}
