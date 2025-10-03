4b03725c3503d6888c5a1eb5b9a3f54ed43211b4
geofrey-eon <geofrey-eon@users.noreply.github.com>
2025-10-03 22:52:55 +0800

    Add user get routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;// Import the controller
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// Route::get('/', function () {
+//     return redirect('/users/12'); // redirect root to a profile for testing
+// });
+// Route::get('/', [UserController::class, 'index']);
+Route::get('/users/{id}', [UserController::class, 'show']);
