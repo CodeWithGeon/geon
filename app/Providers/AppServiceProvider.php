73c0881a1d1a958411345496d52db4c423d4b80e
geofrey-eon <geofrey-eon@users.noreply.github.com>
2025-10-05 19:47:26 +0800

    Bind ProductRepositoryInterface to ProductRepository for controller integration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,15 +3,20 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use App\Repositories\Contracts\ProductRepositoryInterface;
+use App\Repositories\Eloquent\ProductRepository;
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
     public function register(): void
-    {
-        //
+    {   //services from controller to repository
+        // Bind the interface to the concrete class
+        $this->app->bind(
+            ProductRepositoryInterface::class,
+            ProductRepository::class
+        );
     }
 
     /**
