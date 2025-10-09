37ab31769ef123af3a6c3893a622009d279b792b
geofrey-eon <194751426+CodeWithGeon@users.noreply.github.com>
2025-10-10 00:53:41 +0800

    Bound repository interfaces to their Eloquent implementations in AppServiceProvider for dependency injection

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,18 +4,25 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Repositories\Contracts\ProductRepositoryInterface;
+use App\Repositories\Contracts\InventoryRepositoryInterface;
 use App\Repositories\Eloquent\ProductRepository;
+use App\Repositories\Eloquent\InventoryRepository;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
     public function register(): void
-    {   //services from controller to repository
+    {    //services from controller to repository
         // Bind the interface to the concrete class
         $this->app->bind(
             ProductRepositoryInterface::class,
             ProductRepository::class
+        );
+        $this->app->bind(
+            InventoryRepositoryInterface::class,
+            InventoryRepository::class
         );
     }
 
