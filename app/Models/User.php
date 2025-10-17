300bc14aed93d4fb1964d14cd50b69fbe8355d77
geofrey-eon <194751426+CodeWithGeon@users.noreply.github.com>
2025-10-17 18:05:22 +0800

    fillable fields, hidden attributes, casts, isAdmin method, and products relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,48 +1,76 @@
 <?php
 
 namespace App\Models;
-
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-        use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
-     *
      * @var list<string>
      */
-        protected $fillable = [
-            'name',
-            'email',
-            'password',
-        ];
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'is_admin',
+    ];
 
     /**
-     * The attributes that should be hidden for serialization.
+     * hidden
      *
-     * @var list<string>
+     * @var array
      */
-        protected $hidden = [
-            'password',
-            'remember_token',
-        ];
-
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
     /**
      * Get the attributes that should be cast.
-     *
      * @return array<string, string>
      */
-        protected function casts(): array
-        {
-            return [
-                'email_verified_at' => 'datetime',
-                'password' => 'hashed',
-            ];
-        }
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+        'is_admin' => 'boolean',
+    ];
+    /**
+     * Check if the user is an admin
+     * @return bool
+     */
+    public function isAdmin(): bool
+    {
+        return $this->is_admin;
+    }
+
+
+    /**
+     * If you want to track products created by a user
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'created_by');
+    }
+    /**
+     * cast
+     *
+     * @return array
+     */
+    // protected function cast(): array
+    // {
+    //     return
+    //         [
+    //             'email_verified_at' => 'datetime',
+    //             'password' => 'hashed',
+    //             'deleted_at' => 'datetime',
+    //             'is_admin' => 'boolean',
+    //         ];
+    // }
 }
