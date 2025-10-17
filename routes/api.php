fbd2fbc2163679316e94dbcba41eb701d1eb9e3a
geofrey-eon <194751426+CodeWithGeon@users.noreply.github.com>
2025-10-17 18:20:31 +0800

    authentication and product management with Sanctum and admin middleware

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('auth')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::middleware('auth:sanctum')->post('logout', [AuthController::class, 'logout']);
+});
+
+Route::get('products', [ProductController::class, 'index']);
+Route::get('products/{id}', [ProductController::class, 'show']);
+
+
+Route::middleware(['auth:sanctum', 'admin'])->prefix('products')->group(function () {
+    Route::post('/', [ProductController::class, 'store']);
+    Route::put('{id}', [ProductController::class, 'update']);
+    Route::delete('{id}', [ProductController::class, 'destroy']);
+    Route::post('{id}/restore', [ProductController::class, 'restore']);
+    Route::get('{id}/edit', [ProductController::class, 'edit']);
+});
