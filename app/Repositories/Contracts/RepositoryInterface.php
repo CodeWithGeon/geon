ee9a32f008155715bad491c1017a6daf93d1b1c1
geofrey-eon <194751426+CodeWithGeon@users.noreply.github.com>
2025-10-17 18:10:41 +0800

    base RepositoryInterface with common CRUD method definitions

--- app/Repositories/Contracts/RepositoryInterface.php
+++ app/Repositories/Contracts/RepositoryInterface.php
@@ -5,36 +5,12 @@ namespace App\Repositories\Contracts;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
-
-
-//A Repository Interface defines what methods exist. (no logics)
 interface RepositoryInterface
 {
-   /**
-    * Get all records.
-    */
-   public function all(): Collection;
-
-   /**
-    * Find a record by its ID.
-    */
-   public function find($id): ?Model;
-
-   /**
-    * Create a new record.
-    */
-   public function create(array $attributes): Model;
-
-   /**
-    * Update a record by its ID.
-    */
-   public function update($id, array $attributes): bool;
-
-   /**
-    * Delete existing resource.
-    *
-    * @param int $id
-    * @return bool
-    */
-   public function delete(int $id): bool;
+    public function all(): Collection;
+    public function find($id): ?Model;
+    public function create(array $data): Model;
+    public function update($id, array $attributes): bool;
+    public function delete(int $id): bool;
+    public function restore($id);
 }
