ae3c4d38397bdf4b79e0789efd373505d31eaac9
geofrey-eon <194751426+CodeWithGeon@users.noreply.github.com>
2025-11-01 14:09:04 +0800

    initial commit

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,19 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
-//API controllers handle JSON responses and status codes.
+use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\OrderItemController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Api\CategoryController;
+
 Route::prefix('auth')->group(function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
     Route::middleware('auth:sanctum')->post('logout', [AuthController::class, 'logout']);
 });
 
+// Public product routes
 Route::get('products', [ProductController::class, 'index']);
 Route::get('products/{id}', [ProductController::class, 'show']);
 
@@ -20,4 +24,32 @@ Route::middleware(['auth:sanctum', 'admin'])->prefix('products')->group(function
     Route::delete('{id}', [ProductController::class, 'destroy']);
     Route::post('{id}/restore', [ProductController::class, 'restore']);
     Route::get('{id}/edit', [ProductController::class, 'edit']);
+});
+
+
+Route::middleware(['auth:sanctum'])->prefix('orders')->group(function () {
+    Route::post('/', [OrderController::class, 'store']);
+    Route::get('/user/{userId}', [OrderController::class, 'userOrders']);
+});
+
+
+Route::middleware(['auth:sanctum', 'admin'])->prefix('orders')->group(function () {
+    Route::get('/', [OrderController::class, 'index']);
+    Route::get('/status/{status}', [OrderController::class, 'ByStatus']);
+    Route::get('/{id}', [OrderController::class, 'show']);
+});
+
+
+Route::middleware(['auth:sanctum'])->prefix('order-items')->group(function () {
+    Route::post('/', [OrderItemController::class, 'store']);
+    Route::get('/order/{orderId}', [OrderItemController::class, 'getItemsByOrder']);
+    Route::get('/product/{productId}', [OrderItemController::class, 'getItemByProduct']);
+});
+
+Route::get('categories', [CategoryController::class, 'index']);
+Route::get('categories/{id}', [CategoryController::class, 'show']);
+Route::middleware(['auth:sanctum', 'admin'])->prefix('categories')->group(function () {
+    Route::post('/', [CategoryController::class, 'store']);
+    Route::put('{id}', [CategoryController::class, 'update']);
+    Route::delete('{id}', [CategoryController::class, 'destroy']);
 });
