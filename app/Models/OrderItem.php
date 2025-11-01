ba9e637d877c5bcb67bfd696fd9a44df42a546df
geofrey-eon <194751426+CodeWithGeon@users.noreply.github.com>
2025-11-01 14:03:25 +0800

    initial commit

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'product_id',
+        'quantity',
+        'unit_price',
+        'subtotal',
+    ];
+
+    /**
+     * order
+     * the order this item belongs to
+     * @return
+     */
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    /**
+     * the product associated with this order item
+     * @return
+     */
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
