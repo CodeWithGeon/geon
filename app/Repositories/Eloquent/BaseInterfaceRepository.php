32b8be785e8db5281ae6f603aa57743e68a88ea3
Geofrey <194751426+CodeWithGeon@users.noreply.github.com>
2025-11-01 14:15:35 +0800

    Update BaseInterfaceRepository.php

--- app/Repositories/Eloquent/BaseInterfaceRepository.php
+++ app/Repositories/Eloquent/BaseInterfaceRepository.php
@@ -4,5 +4,5 @@ namespace App\Repositories\Eloquent;
 
 use App\Repositories\Contracts\BaseInterfaceRepositoryInterface;
 
-class BaseInterfaceRepository extends BaseRepository // implements BaseInterfaceRepositoryInterface
+class BaseInterfaceRepository extends BaseRepository 
 {}
