0e1efeb5efa0d63d3df08f1c97828289c8ba7e53
geofrey-eon <194751426+CodeWithGeon@users.noreply.github.com>
2025-11-01 14:13:30 +0800

    initial commit

--- app/Repositories/Eloquent/BaseInterfaceRepository.php
+++ app/Repositories/Eloquent/BaseInterfaceRepository.php
@@ -4,13 +4,5 @@ namespace App\Repositories\Eloquent;
 
 use App\Repositories\Contracts\BaseInterfaceRepositoryInterface;
 
-class BaseInterfaceRepository extends BaseRepository implements BaseInterfaceRepositoryInterface
-{
-    // Handles data access (CRUD, DB queries)
-    //Generic CRUD logic (shared by all repositories)
-    //common reusable Eloquent logic
-    // BaseRepository is an abstract class that holds all the common CRUD logic shared across all your repositories
-
-    //Query Builder is good for complex joins, aggregates, or raw performance queries.
-    //your base repo defines generic CRUD, and each specific repo (like ProductRepo) adds custom query logic using either Eloquent or Query Builder.
-}
+class BaseInterfaceRepository extends BaseRepository // implements BaseInterfaceRepositoryInterface
+{}
