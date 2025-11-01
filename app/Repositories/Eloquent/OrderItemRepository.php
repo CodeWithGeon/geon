4b15ea0f3e4ff04a352608a3578e5623d97f448f
geofrey-eon <194751426+CodeWithGeon@users.noreply.github.com>
2025-11-01 14:05:00 +0800

    initial commit

---
+++ app/Repositories/Eloquent/OrderItemRepository.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+use App\Models\OrderItem;
+use App\Repositories\Contracts\OrderItemRepositoryInterface;
+use Illuminate\Database\Eloquent\Collection;
+
+class OrderItemRepository extends BaseRepository implements OrderItemRepositoryInterface
+{
+    public function __construct(OrderItem $model)
+    {
+        parent::__construct($model);
+    }
+
+    public function getItemsByOrder(int $orderId): Collection
+    {
+        return $this->model->with('product')
+            ->where('order_id', $orderId)
+            ->get();
+    }
+
+    public function getItemsByProduct(int $productId): Collection
+    {
+        return $this->model->with('order')
+            ->where('product_id', $productId)
+            ->get();
+    }
+
+    public function createItem(array $data): OrderItem
+    {
+        return $this->model->create($data);
+    }
+}
