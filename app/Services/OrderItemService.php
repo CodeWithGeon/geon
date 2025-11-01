05c659e64d44bdd071fc921c5b3124fb2e9b5d7f
geofrey-eon <194751426+CodeWithGeon@users.noreply.github.com>
2025-11-01 14:05:34 +0800

    initial commit

---
+++ app/Services/OrderItemService.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\OrderItem;
+use App\Repositories\Contracts\OrderItemRepositoryInterface;
+use Illuminate\Database\Eloquent\Collection;
+
+use Illuminate\Support\Facades\Auth;
+
+
+class OrderItemService
+{
+    protected OrderItemRepositoryInterface $orderItemRepository;
+
+    public function __construct(OrderItemRepositoryInterface $orderItemRepository)
+    {
+        $this->orderItemRepository = $orderItemRepository;
+    }
+
+    /**
+     * createOrderItem
+     *
+     * @param  mixed $data
+     * @return OrderItem
+     */
+    public function createOrderItem(array $data): OrderItem
+    {
+        $attributes = collect($data)
+            ->only(['order_id', 'product_id', 'quantity', 'unit_price'])
+            ->toArray();
+
+        // Automatically compute subtotal
+        $attributes['subtotal'] = $attributes['quantity'] * $attributes['unit_price'];
+
+
+        $attributes['created_by'] = Auth::id();
+
+        return $this->orderItemRepository->createItem($attributes);
+    }
+
+    /**
+     * getItemsByOrder
+     *
+     * @param  mixed $orderId
+     * @return Collection
+     */
+    public function getItemsByOrder(int $orderId): Collection
+    {
+        return $this->orderItemRepository->getItemsByOrder($orderId);
+    }
+
+
+    /**
+     * getItemByProduct
+     *
+     * @param  mixed $productId
+     * @return Collection
+     */
+    public function getItemByProduct(int $productId): Collection
+    {
+        return $this->orderItemRepository->getItemsByProduct($productId);
+    }
+}
