e039aa2706e031372575c69251a4c3e415fe8749
geofrey-eon <194751426+CodeWithGeon@users.noreply.github.com>
2025-11-04 00:47:59 +0800

    Changes Routes

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::prefix('auth')->group(function () {
 Route::get('products', [ProductController::class, 'index']);
 Route::get('products/{id}', [ProductController::class, 'show']);
 
-
+//products
 Route::middleware(['auth:sanctum', 'admin'])->prefix('products')->group(function () {
     Route::post('/', [ProductController::class, 'store']);
     Route::put('{id}', [ProductController::class, 'update']);
@@ -32,20 +32,21 @@ Route::middleware(['auth:sanctum'])->prefix('orders')->group(function () {
     Route::get('/user/{userId}', [OrderController::class, 'userOrders']);
 });
 
-
+//order
 Route::middleware(['auth:sanctum', 'admin'])->prefix('orders')->group(function () {
     Route::get('/', [OrderController::class, 'index']);
     Route::get('/status/{status}', [OrderController::class, 'ByStatus']);
     Route::get('/{id}', [OrderController::class, 'show']);
 });
 
-
+// orderItem
 Route::middleware(['auth:sanctum'])->prefix('order-items')->group(function () {
     Route::post('/', [OrderItemController::class, 'store']);
     Route::get('/order/{orderId}', [OrderItemController::class, 'getItemsByOrder']);
     Route::get('/product/{productId}', [OrderItemController::class, 'getItemByProduct']);
+    Route::get('/user/{userId}', [OrderItemController::class, 'getItemsByUser']);
 });
-
+//category
 Route::get('categories', [CategoryController::class, 'index']);
 Route::get('categories/{id}', [CategoryController::class, 'show']);
 Route::middleware(['auth:sanctum', 'admin'])->prefix('categories')->group(function () {
