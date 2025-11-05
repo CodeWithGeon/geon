d7153ac0730eb6d3401fe233604e4b142af0963c
geofrey-eon <194751426+CodeWithGeon@users.noreply.github.com>
2025-11-05 23:50:42 +0800

    changes

--- app/Repositories/Contracts/ProductRepositoryInterface.php
+++ app/Repositories/Contracts/ProductRepositoryInterface.php
@@ -6,14 +6,13 @@ use App\Models\Product;
 use Illuminate\Database\Eloquent\Collection;
 
 //inherits all CRUD method definitions (all, find, create, update, delete) from the base RepositoryInterface.
-//defines methods for accessing product data specific to the Product model.
+
 interface ProductRepositoryInterface extends RepositoryInterface
 {
     public function getAvailableProducts(): Collection;
-    public function findByName(string $name): ?Product;
     public function createProduct(array $data): Product;
     public function findByCategory($categoryId): Collection;
+    public function findByName(string $name): ?Product;
     public function lowStock(int $limit = 10): Collection;
-    public function restore($id);
-    public function getProductById(int $id): ?Product;
+
 }
