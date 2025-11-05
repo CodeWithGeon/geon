4cb5ee6502450e5dc85b0ac856ed3489a7984e25
geofrey-eon <194751426+CodeWithGeon@users.noreply.github.com>
2025-11-05 23:52:48 +0800

    changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,8 +11,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            CategorySeeder::class,
             ProductSeeder::class,
             UserSeeder::class,
+
         ]);
     }
 }
